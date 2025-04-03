4c239b90e38c01a682d3c84f28c6d4d3ecd5f3c6
ncaughron17 <143098908+ncaughron17@users.noreply.github.com>
2025-04-03 03:36:40 -0400

    adding in some html tests and initial commit

--- src/routes/hobby2.tsx
+++ src/routes/hobby2.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import Sax from  '../public/images.jpeg';
+import Bass from  '../public/415ZGxk58XL.jpg';
+import Abletonlogo from '../public/meg6nawssnb01.jpg';
+import Song from '../public/Chenso Club-esque (1).wav'
 
 const Hobby2: React.FC = () => {
     return (
-        <div>
-            <h1>Our Combined Web Page</h1>
+        <div style={{fontFamily:"Georgia", background:"teal", padding:"100px",height:"1200px"}}>
+            <h1 style={{position:"sticky", textAlign:'center', top:70, width:416, backgroundColor:"grey"}}>Nate's Hobby</h1><br/>
+            <h2 style={{fontSize:40}}>Music!</h2>
+
+            <div id={"imgRow"} style={{flexWrap:'wrap'}}>
+                <img src={Sax} style={{float:"left", width:"50%"}}/>
+                <img src={Bass} style={{float:"right", width:"50%"}}/>
+                <img src={Abletonlogo} style={{float:"right", width:"50%"}}/>
+            </div>
+
+
 
         </div>
+
     );
 };
 
